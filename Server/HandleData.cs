46abfbc821d90b1501666ce34a272bfe8dfd3e63
Steven M. Fortune <sfortune@live.com>
2017-09-11 18:40:45 -0500

    Inital Commit

---
+++ Server/HandleData.cs
@@ -0,0 +1,85 @@
+﻿using Lidgren.Network;
+using static System.Console;
+using static System.Threading.Thread;
+
+namespace Server
+{
+    public class HandleData
+    {
+        public void HandleDataMessage(NetServer g_Server)
+        {
+            NetIncomingMessage incMSG;
+
+            if ((incMSG = g_Server.ReadMessage()) != null)
+            {
+                switch (incMSG.MessageType)
+                {
+                    case NetIncomingMessageType.DiscoveryRequest:
+                        HandleDiscoveryRequest(incMSG, g_Server);
+                        break;
+
+                    case NetIncomingMessageType.ConnectionApproval:
+                        HandleConnectionApproval(incMSG, g_Server);
+                        break;
+
+                    case NetIncomingMessageType.StatusChanged:
+                        HandleStatusChange(incMSG, g_Server);
+                        break;
+
+                    case NetIncomingMessageType.Data:
+                        switch(incMSG.ReadByte())
+                        {
+
+                        }
+                        break;
+                }
+            }
+            g_Server.Recycle(incMSG);
+        }
+
+        private void HandleDiscoveryRequest(NetIncomingMessage incMSG, NetServer g_Server)
+        {
+            WriteLine("Client Discovered @ " + incMSG.SenderEndPoint.ToString());
+            NetOutgoingMessage outMSG = g_Server.CreateMessage();
+            outMSG.Write("Scorched Server");
+            g_Server.SendDiscoveryResponse(outMSG, incMSG.SenderEndPoint);
+        }
+
+        private void HandleConnectionApproval(NetIncomingMessage incMSG, NetServer g_Server)
+        {
+            if (incMSG.ReadByte() == (byte)Packet.Connection)
+            {
+                string Connect = incMSG.ReadString();
+                if (Connect == "scorched")
+                {
+                    incMSG.SenderConnection.Approve();
+                    Sleep(500);
+                    NetOutgoingMessage outMSG = g_Server.CreateMessage();
+                    outMSG.Write((byte)Packet.Connection);
+                    g_Server.SendMessage(outMSG, incMSG.SenderConnection, NetDeliveryMethod.ReliableOrdered);
+                }
+                else { incMSG.SenderConnection.Deny(); }
+            }
+        }
+
+        private void HandleStatusChange(NetIncomingMessage incMSG, NetServer g_Server)
+        {
+            WriteLine(incMSG.SenderConnection.ToString() + " status changed. " + incMSG.SenderConnection.Status);
+            if (incMSG.SenderConnection.Status == NetConnectionStatus.Disconnected || incMSG.SenderConnection.Status == NetConnectionStatus.Disconnecting)
+            {
+                WriteLine("Disconnected, clearing data...");
+                //Clear data
+                WriteLine("Data cleared, connection now open.");
+            }
+        }
+    }
+
+    public enum Packet : byte
+    {
+        Connection,
+        Register,
+        Login,
+        Error,
+        Notification
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Lidgren.Network;
+using static System.Console;
+using static System.Environment;
+using static System.Threading.Thread;
+
+namespace Server
+{
+    class Program
+    {
+        static NetServer g_Server;
+        static NetPeerConfiguration g_Config;
+
+        static void Main(string[] args)
+        {
+            Title = "Scorched Server";
+            WriteLine("Loading Please Wait...");
+            g_Config = new NetPeerConfiguration("scorched") { Port = 14242 };
+            g_Config.EnableMessageType(NetIncomingMessageType.ConnectionApproval);
+            g_Config.EnableMessageType(NetIncomingMessageType.ConnectionLatencyUpdated);
+            g_Config.EnableMessageType(NetIncomingMessageType.DiscoveryRequest);
+            g_Config.UseMessageRecycling = true;
+            g_Config.MaximumConnections = GlobalVariables.MAX_PLAYERS;
+            g_Config.EnableUPnP = false;
+            g_Config.ConnectionTimeout = 25.0f;
+            g_Server = new NetServer(g_Config);
+            g_Server.Start();
+            Server s_Server = new Server();
+            s_Server.Loop(g_Server);
+        }
+    }
+
+    class Server
+    {
+        static int lastTick;
+        static int lastFrameRate;
+        static int frameRate;
+        HandleData g_HandleData;
+
+        public void Loop(NetServer g_Server)
+        {
+            g_HandleData = new HandleData();
+
+            WriteLine("Listening for connections...");
+            while (true)
+            {
+                Title = "Scorched - CPS: " + CalculateCyclesPerSecond();
+                g_HandleData.HandleDataMessage(g_Server);
+                Sleep(10);
+            }
+        }
+
+        static int CalculateCyclesPerSecond()
+        {
+            if (TickCount - lastTick >= 1000)
+            {
+                lastFrameRate = frameRate;
+                frameRate = 0;
+                lastTick = TickCount;
+            }
+            frameRate++;
+            return lastFrameRate;
+        }
+    }
+
+    public static class GlobalVariables
+    {
+        public const int MAX_PLAYERS = 10;
+    }
+}
